2565ec3a3dffc14b5f67eec58f37b4f4cff68cc6
Ricardo Bautista <37841371+rickyhaswifi@users.noreply.github.com>
2021-06-12 08:04:48 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,137 +1,104 @@
-// var locationValue = document.getElementById("Select-Menu-Location")
-// locationValue.addEventListener('change', function() {
-//   document.getElementById("LocationOutput").innerHTML = locationValue.value;
-// })
-
-// const search = document.getElementById('search')
-// search.addEventListener('input', () => searchList(search.value))
-
 // ID of the Google Spreadsheet
- var spreadsheetID = "1PQJSCErV-r-yxTuYxYzMfWLX3ncboRJpLNU_aWatF-4";
- 
- // Make sure it is public or set to Anyone with link can view 
- var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
- 
-// make JSON call to Google Data API
-$.getJSON(url, function(data, locationValue) {
-
-  // set global html variable
-  var html = '';
-
-  // var locationValue = document.getElementById("mySelect");
-  // locationValue.addEventListener('change', function myFunction() {
-  //   console.log(locationValue.value);
-  // })
-// function myFunction() {
-// }
-
-  // build table headings
-
-  html += '<div class="card--container test-list">';
-
-  var entry = data.feed.entry;
-
-  var locationValue = document.getElementById("Select-Menu-Location")
-  locationValue.addEventListener('change', function() {
-    document.getElementById("LocationOutput").innerHTML = locationValue.value;
-    locationFiltered = entry.filter( function(person) {
-      return (person.gsx$location.$t === locationValue.value)
-      
+var spreadsheetID = "1PQJSCErV-r-yxTuYxYzMfWLX3ncboRJpLNU_aWatF-4";
+// Make sure it is public or set to Anyone with link can view 
+var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+var peopleData;
+var selectedLocation;
+var selectedRole;
+var locationOptions;
+var roleOptions;
+$.getJSON(url, function(data) {
+  peopleData = data.feed.entry;
+  const locations = new Set();
+  const roles = new Set();
+  for (var i = 0; i < peopleData.length; i++) {
+    locations.add(peopleData[i]['gsx$location']['$t']);
+    roles.add(peopleData[i]['gsx$role']['$t']);
+  }
+  locationOptions = Array.from(locations).sort();
+  roleOptions = Array.from(roles).sort();
+  locationOptions.unshift('-');
+  roleOptions.unshift('-');
+  createLocationSelect();
+  createRoleSelect();
+  renderPeople();
+});
+function createLocationSelect() {
+  $('#Select-Menu-Location').append(
+    $(document.createElement('label')).prop({
+        for: 'Location'
     })
-    for (var i = 0; i < locationFiltered.length; i++) {
-      html += `
-      <div class="card-item">
-        <img src="${locationFiltered[i]['gsx$img']['$t']}"/>
-        <h4>${locationFiltered[i]['gsx$name']['$t']}</h4>
-        <h4>Location: ${locationFiltered[i]['gsx$location']['$t']}</h4>
-        <h4>Role: ${locationFiltered[i]['gsx$role']['$t']}</h4>
-      </div>
-      `;
-    }
-    html += '</div>';
+  ).append(
+    $(document.createElement('select')).prop({
+        id: 'locationSelect',
+        name: 'location'
+    })
+  )
+  for (const l of locationOptions) {
+    $('#locationSelect').append($(document.createElement('option')).prop({
+        value: l,
+        text: l
+    }))
+  }
+  const locationSelect = document.getElementById("locationSelect")
+  locationSelect.addEventListener('change', function(e) {
+    const selected = locationOptions[e.target.selectedIndex];
+    selectedLocation = selected === '-' ? null : selected;
+    renderPeople();
   })
-  // })
-  // Location Filter
- 
-  for (var i = 0; i < entry.length; i++) {
+}
+function createRoleSelect() {
+  $('#Select-Menu-Role').append(
+    $(document.createElement('label')).prop({
+        for: 'Role Type'
+    })
+  ).append(
+    $(document.createElement('select')).prop({
+        id: 'roleSelect',
+        name: 'role'
+    })
+  )
+  for (const r of roleOptions) {
+    $('#roleSelect').append($(document.createElement('option')).prop({
+        value: r,
+        text: r
+    }))
+  }
+  const roleSelect = document.getElementById("roleSelect")
+  roleSelect.addEventListener('change', function(e) {
+    const selected = roleOptions[e.target.selectedIndex];
+    selectedRole = selected === '-' ? null : selected;
+    renderPeople();
+  })
+}
+function renderPeople() {
+  const filteredPeople = peopleData.filter( function(person) {
+    if (selectedLocation && selectedRole) return (person.gsx$location.$t === selectedLocation && person.gsx$role.$t === selectedRole)
+    else if (selectedLocation) return person.gsx$location.$t === selectedLocation
+    else if (selectedRole) return person.gsx$role.$t === selectedRole
+    else return person
+  });
+  let html = '<div class="card--container test-list">';
+  for (var i = 0; i < filteredPeople.length; i++) {
     html += `
     <div class="card-item">
-      <img src="${entry[i]['gsx$img']['$t']}"/>
-      <h4>${entry[i]['gsx$name']['$t']}</h4>
-      <h4>Location: ${entry[i]['gsx$location']['$t']}</h4>
-      <h4>Role: ${entry[i]['gsx$role']['$t']}</h4>
+    <img src="${filteredPeople[i]['gsx$img']['$t']}"/>
+    <h4>${filteredPeople[i]['gsx$name']['$t']}, ${filteredPeople[i]['gsx$notes']['$t']}</h4>
+    <h4>Location: ${filteredPeople[i]['gsx$location']['$t']}</h4>
+    <h4>Role: ${filteredPeople[i]['gsx$role']['$t']}</h4>
     </div>
     `;
   }
   html += '</div>';
-  
-  // Display
-  // for (var i = 0; i < locationFiltered.length; i++) {
-  //   html += `
-  //   <div class="card-item">
-  //     <img src="${locationFiltered[i]['gsx$img']['$t']}"/>
-  //     <h4>${locationFiltered[i]['gsx$firstname']['$t']}</h4>
-  //     <h4>Location: ${locationFiltered[i]['gsx$location']['$t']}</h4>
-  //     <h4>Role: ${locationFiltered[i]['gsx$type']['$t']}</h4>
-  //   </div>
-  //   `;
-  // }
-  // html += '</div>';
-  
-  // const search = document.getElementById('search')
-  // search.addEventListener('keyup', function searchList(search) {
-  //   console.log(search.value)
-  // })
-    // if (search.value < 1 ) {
-    //   document.getElementById('searchOutput').innerHTML = search.value
-    // }
-    // else {
-    //   for (var i = 0; i < entry.length; i++) {
-    //     var locationValue = document.getElementById("mySelect").value;
-    //     // const locationValues = document.getElementById("mySelect").value;
-    //     console.log(
-    //       (entry[i]['gsx$location']['$t'] === locationValue) ? null : locationValue
-    //     )
-    //   html += `
-    //   <div class="card-item">
-    //     <img src="${entry[i]['gsx$img']['$t']}"/>
-    //     <h4>${entry[i]['gsx$firstname']['$t']}</h4>
-    //     <h4>Location: ${entry[i]['gsx$location']['$t']}</h4>
-    //     <h4>Role: ${entry[i]['gsx$type']['$t']}</h4>
-    //   </div>
-    //   `;
-    // }
-    // html += '</div>';
-    // }
-  
-    // for (var i = 0; i < entry.length; i++) {
-      // var locationValue = document.getElementById("mySelect").value;
-      // // const locationValues = document.getElementById("mySelect").value;
-      // console.log(
-      //   (entry[i]['gsx$location']['$t'] === locationValue) ? null : locationValue
-      // )
-    // html += `
-    // <div class="card-item sort">
-    //   <img src="${entry[i]['gsx$img']['$t']}"/>
-    //   <h4 class="name">${entry[i]['gsx$firstname']['$t']}</h4>
-    //   <h4 class="location">Location: ${entry[i]['gsx$location']['$t']}</h4>
-    //   <h4>Role: ${entry[i]['gsx$type']['$t']}</h4>
-    // </div>
-    // `;
-  // }
-  // html += '</div>';
-
-  // output html
   $('.people-api').html(html);
-});
-
+}
 // loading animation
 var loading = $('.loading');
 loading.hide();
 $(document)
-  .ajaxStart(function() {
-    loading.show();
-  })
-  .ajaxStop(function() {
-    loading.hide();
-  });
+ .ajaxStart(function() {
+   loading.show();
+ })
+ .ajaxStop(function() {
+   loading.hide();
+});
